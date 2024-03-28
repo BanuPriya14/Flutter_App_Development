794a5241adcfcf3234bf84bd969464552b8e8cfb
Banu <banugopal14@gmail.com>
2024-03-27 19:28:18 -0600

    mi_card_flutter_version1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,9 +15,84 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Container(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Banu.jpg'),
+              ),
+              Text(
+                'Banu Gopal',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans 3'
+                ),
+              ),
+              Text(
+                'SOFTWARE DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans 3'
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                //padding: EdgeInsets.all(10.0),
+                //color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                  leading:Icon(
+                    Icons.phone,
+//size: 50.0,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '720-314-2126',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans 3',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )
+              ),
+              Card(
+                //padding: EdgeInsets.all(10.0),
+                //color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                  leading:Icon(
+                    Icons.email,
+//size: 50.0,
+                    color: Colors.teal,
+                  ) ,
+                  title: Text(
+                    'banugus@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans 3',
+                      fontSize: 20.0,
+                    ),
 
+                  ),
+                )
+              )
+            ],
+          )
         ),
       ),
     );
